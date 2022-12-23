648043f6691ac486afdb7d8587c189da16322566
thais de rodez <thaisderodez@gmail.com>
2022-12-23 17:13:19 +0100

    fix(Bills): delete console log

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -142,7 +142,6 @@ export default class {
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0;
     if (this.index === undefined || this.index !== index) this.index = index;
-    console.log("show counter", this.counter);
     if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: "rotate(0deg)" });
       $(`#status-bills-container${this.index}`).html(
