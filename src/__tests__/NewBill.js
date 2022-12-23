b3186045d9f17bdbfd28feaa301c896ef80a3405
thais de rodez <thaisderodez@gmail.com>
2022-12-23 11:22:00 +0100

    feat(newbill): test new bill UI

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,17 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { screen } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+import NewBillUI from "../views/NewBillUI.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+    test("Then the new bill form should be display", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
       //to-do write assertion
-    })
-  })
-})
+      expect(screen.getByTestId("form-new-bill")).toBeInTheDocument();
+    });
+  });
+});
